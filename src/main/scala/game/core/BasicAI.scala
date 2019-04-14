023e4cfa0d70fe7544fa4e1c7fce78ed89e98c7e
Tommi Räsänen <tommi.rasanen@aalto.fi>
2019-04-15 00:27:15 +0300

    Improve AI behaviour

--- src/main/scala/game/core/BasicAI.scala
+++ src/main/scala/game/core/BasicAI.scala
@@ -8,49 +8,57 @@ import scala.util.{Failure, Success}
 
 class BasicAI extends PlayerAI {
 
-  var previousMoveTime: Long = 0
   val calculating = new AtomicBoolean(false)
 
   override def makeMove(game: Game): Boolean = {
-    if (!calculating.get) {
-      val movableCharacters = game.playerList(game.currentPlayer).characters.filter(_.movementPoints > 0)
-      if (!movableCharacters.isEmpty) {
-        val nextToMove = movableCharacters.head
+    val movableCharacters = game.playerList(game.currentPlayer).characters.filter(_.movementPoints > 0)
+    if (!movableCharacters.isEmpty) {
+      val nextToMove = movableCharacters.head
 
-        val characters = game.playerList(game.currentPlayer).characters.filter(_ != nextToMove)
-        val targets = game.playerList.filter(_ != game.playerList(game.currentPlayer)).flatMap(_.characters)
+      val characters = game.playerList(game.currentPlayer).characters.filter(_ != nextToMove)
+      val targets = game.playerList.filter(_ != game.playerList(game.currentPlayer)).flatMap(_.characters)
 
-        if (!targets.isEmpty) {
-          calculating.set(true)
-          Future {
-            val distances = game.distancesToTargets(nextToMove.position, targets.map(_.position), characters)
-            val targetIndex = distances.zipWithIndex.min._2
-            if (distances(targetIndex) != Int.MaxValue) {
-              game.moveCharacter(nextToMove, targets(targetIndex).position)
-            } else {
-              nextToMove.endTurn()
-            }
-          }.onComplete {
-            case Success(result) => {
-              previousMoveTime = System.currentTimeMillis
-              calculating.set(false)
-            }
-            case Failure(t) => {
-              nextToMove.endTurn()
-              calculating.set(false)
-              throw(t)
-            }
+      if (!targets.isEmpty) {
+        calculating.set(true)
+        Future {
+          var priorities = game.distancesToTargets(nextToMove.position, targets.map(_.position), characters).zipWithIndex.filter(_._1 != Int.MaxValue)
+          priorities = priorities.map({ case (distance: Int, i: Int) => {
+            // equalize all targets that we can reach this round
+            var priority = Math.max(distance - nextToMove.movementPoints, 0)
+            if (priority > 0) priority += 150 //add some additional penalty for further characters so closer targets are preferred
+
+            // adjust priority according to how likely the character is to kill the target
+            val targetCharacter = targets(i)
+            var avgDamage = (nextToMove.attackPower * 0.90 - targetCharacter.defensePower).toInt
+            avgDamage = (avgDamage * nextToMove.characterClass.attackBonus.getOrElse(targetCharacter.characterClass.toString, 1.0)).toInt
+            priority += Math.max(targetCharacter.hitpoints - avgDamage, 0)
+            (priority, i)
+          }})
+
+          if (!priorities.isEmpty) {
+            val targetIndex = priorities.min._2
+            game.moveCharacter(nextToMove, targets(targetIndex).position)
+          } else {
+            nextToMove.endTurn()
           }
-        } else {
-          nextToMove.endTurn()
+        }.onComplete {
+          case Success(result) => {
+            calculating.set(false)
+          }
+          case Failure(t) => {
+            nextToMove.endTurn()
+            calculating.set(false)
+            throw(t)
+          }
         }
-        false
       } else {
-        previousMoveTime = System.currentTimeMillis
-        true
+        nextToMove.endTurn()
       }
-    } else {
       false
+    } else {
+      true
     }
   }
+
+  override def isThinking: Boolean = calculating.get
 }
--- src/main/scala/game/core/PlayerAI.scala
+++ src/main/scala/game/core/PlayerAI.scala
@@ -1,18 +1,35 @@
 package game.core
 
-trait PlayerAI {
-
-  var previousMoveTime: Long
+abstract class PlayerAI {
+  var previousMoveTime: Long = PlayerAI.UndefinedMoveTime
   //returns true if done playing turn, false otherwise
-  def playTurn(game: Game): Boolean = {
-    if (shouldMakeMove(game)) makeMove(game) else false
+  //passes control to actual AI if the game allows actions and at least MinMovementDelay has passed since actions were reallowed
+  //wait MinMovementDelay to prevent too rapid AI actions (e.g. attacking with several characters in a split second)
+  final def playTurn(game: Game): Boolean = {
+    if (shouldMakeMove(game)) {
+      if (previousMoveTime == PlayerAI.UndefinedMoveTime) {
+        previousMoveTime = System.currentTimeMillis()
+        false
+      } else if (System.currentTimeMillis - previousMoveTime > PlayerAI.MinMovementDelay) {
+        val result = makeMove(game)
+        previousMoveTime = PlayerAI.UndefinedMoveTime
+        result
+      } else {
+        false
+      }
+    } else {
+      false
+    }
   }
 
-  def shouldMakeMove(game: Game): Boolean = (System.currentTimeMillis - previousMoveTime > PlayerAI.MinMovementDelay) && game.actionsAllowed
+  final def shouldMakeMove(game: Game): Boolean = game.actionsAllowed && !isThinking
 
   def makeMove(game: Game): Boolean
+
+  def isThinking: Boolean
 }
 
 object PlayerAI {
+  val UndefinedMoveTime: Long = -1
   val MinMovementDelay = 500
 }
